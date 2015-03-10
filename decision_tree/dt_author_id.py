9d936e17f65af2d05cd60397298335cfbb11ac05
xu-hong <xuhong713@gmail.com>
2015-03-09 22:17:18 -0400

    add files for decision tree

---
+++ decision_tree/dt_author_id.py
@@ -0,0 +1,43 @@
+# %load dt_author_id.py
+#!/usr/bin/python
+
+""" 
+    this is the code to accompany the Lesson 3 (decision tree) mini-project
+
+    use an DT to identify emails from the Enron corpus by their authors
+    
+    Sara has label 0
+    Chris has label 1
+
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+clf = DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+print accuracy_score(labels_test, pred)
+
+#########################################################
+
